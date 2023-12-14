b44be2ffc303cab1aaf61895da4363ce7f5b0d6c
frameworkfuturistic <frameworkfuturistic@gmail.com>
2023-12-14 11:18:29 +0530

    API key implementation

--- routes/api.php
+++ routes/api.php
@@ -20,12 +20,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-/**
- * | Controller - 01
- */
-Route::controller(MasterController::class)->group(function () {
-    Route::post('master/v1/get-dept-by-hospid', 'getDepartsByHospitalId');  // 01
-    Route::post('master/v1/get-doctors-by-hospdept', 'getConsultantsByHospDeptId');  // 02
-    Route::post('master/v1/get-shifts-by-hospconsultant', 'getShiftsByHospDoctorId');  // 03
-    Route::post('master/v1/get-city-by-stateid', 'readCitiesByStates'); // 04
+Route::group(['middleware' => ['api.key']], function () {
+    /**
+     * | Controller - 01
+     */
+    Route::controller(MasterController::class)->group(function () {
+        Route::post('master/v1/get-dept-by-hospid', 'getDepartsByHospitalId');  // 01
+        Route::post('master/v1/get-doctors-by-hospdept', 'getConsultantsByHospDeptId');  // 02
+        Route::post('master/v1/get-shifts-by-hospconsultant', 'getShiftsByHospDoctorId');  // 03
+        Route::post('master/v1/get-city-by-stateid', 'readCitiesByStates'); // 04
+    });
 });
